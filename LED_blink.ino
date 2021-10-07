b94cc9005406f168974fba184d3c513dca1ed391
Saqib Sizan Khan <ssk123098@gmail.com>
2021-10-08 00:15:47 +0600

    task 1 added

---
+++ LED_blink.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT); //[pin no., state: INPUT/OUTPUT]
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH); // [pin no.,value: HIGH/1 or LOW/0]
+}
