09b710f3630fecceefa46effa6eb8e9d36a48ff2
Saqib Sizan Khan <ssk123098@gmail.com>
2021-10-15 19:09:13 +0600

    task 4 added

---
+++ Serial_in_out.ino
@@ -0,0 +1,17 @@
+byte a;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available())
+  {
+    a = Serial.read();
+
+    Serial.write(a);
+    Serial.println();
+  }
+}
